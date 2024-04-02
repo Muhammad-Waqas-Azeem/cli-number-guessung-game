0a597e95ddf6eb2150eafe75cd10aea82ff7d00a
Muhammad Waqas Azeem <160093061+Muhammad-Waqas-Azeem@users.noreply.github.com>
2024-04-02 23:30:51 +0500

    some errors fixed

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
+#! /usr/bin/env node
 import inquirer from "inquirer";
 const randomNumber = Math.round(Math.random() * 10);
 console.log(randomNumber);
-// const userNumber=await inquirer.prompt([{message:"Enter Your Guess B/W 1-100",type:"number",name:"userNum"}]); 
-// console.log(userNumber.userNum,randomNumber);
-// const userNumber=await inquirer.prompt([{message:"Enter Your Guess B/W 1-100",type:"number",name:"userNum"}]); 
 let userNumber;
 do {
     userNumber = await inquirer.prompt([{ message: "Enter Your Guess B/W 1-100", type: "number", name: "userNum" }]);
